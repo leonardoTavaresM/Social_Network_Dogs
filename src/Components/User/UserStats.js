f58fd3038a2457b0589f59b91b48f138a6a2820e
leonardoTavaresM <leomaltadesa0712@gmail.com>
2023-02-26 15:29:47 -0300

    feat: adding victory lib, userStats and Graphs to project

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { useEffect } from "react";
+import { STATS_GET } from "../../api";
+import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
+import Head from "../Helper/Head";
+import Loading from "../Helper/Loading";
+// import UserStatsGraphs from "./UserStatsGraphs";
+const UserStatsGraphs = React.lazy(() => import("./UserStatsGraphs"));
 
 const UserStats = () => {
-  return <div>Estatisticas do Usuario</div>;
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <React.Suspense fallback={<div></div>}>
+        <Head title="Estatisticas" descriptions="Estatisticas do perfil." />
+        <UserStatsGraphs data={data} />
+      </React.Suspense>
+    );
+  else return null;
 };
 
 export default UserStats;
